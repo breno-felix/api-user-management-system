d7c4fe61fb2d8a8875fd12dd0f61963bc5ebbccf
Breno Felix <brenodev.felix@gmail.com>
2023-04-17 11:55:27 -0300

    :wrench:	chore: config imports and exports in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
