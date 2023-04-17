330baee4ab4773012668012e34f1bd559f794a42
Breno Felix <brenodev.felix@gmail.com>
2023-04-17 17:27:11 -0300

    :wrench:	chore: add AuthController and register the JwtModule as global.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,20 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
+      global: true,
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: process.env.JWT_EXPIRATION },
     }),
   ],
   providers: [AuthService],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
