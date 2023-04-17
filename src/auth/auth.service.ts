35fb2bcb901823d162da32049cb16d6774a6eef4
Breno Felix <brenodev.felix@gmail.com>
2023-04-17 17:24:36 -0300

    :construction: create function signIn and createAccessToken  in AuthService.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { sign } from 'jsonwebtoken';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, password: string): Promise<string> {
+    const user = await this.userService.findByEmail(email);
+    if (await bcrypt.compare(password, user.password)) {
+      throw new UnauthorizedException();
+    }
+    return this.createAccessToken(user._id.toString());
+  }
+
+  async createAccessToken(userId: string): Promise<string> {
+    return this.jwtService.signAsync(userId);
+  }
+}
