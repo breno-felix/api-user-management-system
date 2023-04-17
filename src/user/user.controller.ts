8f99610bd824e16537b37125120c5f7d5f1061b5
Breno Felix <brenodev.felix@gmail.com>
2023-04-17 18:51:09 -0300

    :construction: create function create, findOne, findByEmail, findAll, update and delete in UserController.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { UserDocument } from './schemas/user.schema';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDTO): Promise<UserDocument> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get(':userId')
+  async findOne(@Param('userId') userId: string): Promise<UserDocument> {
+    return this.userService.findOne(userId);
+  }
+
+  @Get('email/:email')
+  async findByEmail(@Param('email') email: string): Promise<UserDocument> {
+    return this.userService.findByEmail(email);
+  }
+
+  @Get()
+  async findAll(): Promise<UserDocument[]> {
+    return this.userService.findAll();
+  }
+
+  @Put(':userId')
+  async update(
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDTO,
+  ): Promise<UserDocument> {
+    return this.userService.update(userId, updateUserDto);
+  }
+
+  @Delete(':userId')
+  async delete(@Param('userId') userId: string): Promise<UserDocument> {
+    return this.userService.delete(userId);
+  }
+}
