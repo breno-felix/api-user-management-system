061d7c07dcf765eff82770b806c7ba96c7a6d5d8
Breno Felix <brenodev.felix@gmail.com>
2023-04-18 10:07:00 -0300

    :wrench:	chore: config UserModule adding forwardRef(() => AuthModule)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import {
+  forwardRef,
   MiddlewareConsumer,
   Module,
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
@@ -13,6 +15,7 @@ import { UserService } from './user.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    forwardRef(() => AuthModule),
   ],
   controllers: [UserController],
   providers: [UserService],
