8466c04ef89e83c129a6e65ddee306d2c2b98133
Breno Felix <brenodev.felix@gmail.com>
2023-04-18 15:14:29 -0300

    :recycle:	refactor: add UnauthorizedException to AuthGuard.

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { UserService } from 'src/user/user.service';
 
@@ -10,16 +16,22 @@ export class AuthGuard implements CanActivate {
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const { authorization } = request.headers;
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
     try {
-      const data = await this.authService.checkToken(
-        (authorization ?? '').split(' ')[1],
-      );
+      const data = await this.authService.checkToken(token);
       request.tokenPayload = data;
       request.user = await this.userService.findOne(data.id);
       return true;
     } catch (error) {
       return false;
     }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
