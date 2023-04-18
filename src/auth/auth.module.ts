e855bacdd18183af744ee6ee9ad1415030237bab
Breno Felix <brenodev.felix@gmail.com>
2023-04-18 14:05:09 -0300

    :heavy_minus_sign:	build: @nestjs/passport, passport and passport-jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -8,7 +7,6 @@ import { AuthController } from './auth.controller';
 @Module({
   imports: [
     forwardRef(() => UserModule),
-    PassportModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
