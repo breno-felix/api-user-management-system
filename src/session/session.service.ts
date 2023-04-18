2de42948673350f6dee89fdb93b7258f6e23ceb8
Breno Felix <brenodev.felix@gmail.com>
2023-04-18 20:23:18 -0300

    :construction: create function create and findLastSessionByUserId in SessionService.

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateSessionDto } from './dto/create-session.dto';
+import { Session, SessionDocument } from './schemas/session.schema';
 
 @Injectable()
-export class SessionService {}
+export class SessionService {
+  constructor(
+    @InjectModel(Session.name)
+    private readonly sessionModel: Model<SessionDocument>,
+  ) {}
+
+  async create(createSessionDto: CreateSessionDto): Promise<SessionDocument> {
+    const createdSession = new this.sessionModel(createSessionDto);
+    return createdSession.save();
+  }
+
+  async findLastSessionByUserId(userId: string): Promise<SessionDocument> {
+    return this.sessionModel.findOne({ userId }).sort({ created_at: -1 });
+  }
+}
