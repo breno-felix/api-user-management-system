7d55b1c08b97380c3df7d7b3dd080f339d7d5a90
Breno Felix <brenodev.felix@gmail.com>
2023-04-18 08:26:13 -0300

    :recycle:	refactor: rename signIn to login and return login function from object to Token interface.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,17 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDTO } from './dto/login-user.dto';
 
+interface Token {
+  accessToken: string;
+}
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Body() loginUserDTO: LoginUserDTO): Promise<string> {
-    return this.authService.signIn(loginUserDTO.email, loginUserDTO.password);
+  async login(@Body() loginUserDTO: LoginUserDTO): Promise<Token> {
+    return this.authService.login(loginUserDTO.email, loginUserDTO.password);
   }
 }
