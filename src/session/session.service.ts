3b9801e9c528a55d85e621ce41013268df567c3c
Breno Felix <brenodev.felix@gmail.com>
2023-04-24 07:10:27 -0300

    :construction: apply SessionsGateway.

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -3,17 +3,22 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { Session, SessionDocument } from './schemas/session.schema';
+import { SessionsGateway } from './session.gateway';
 
 @Injectable()
 export class SessionService {
   constructor(
     @InjectModel(Session.name)
     private readonly sessionModel: Model<SessionDocument>,
+    private readonly sessionsGateway: SessionsGateway,
   ) {}
 
   async create(createSessionDto: CreateSessionDto): Promise<SessionDocument> {
     const createdSession = new this.sessionModel(createSessionDto);
-    return createdSession.save();
+    const session = await createdSession.save();
+    const sessions = await this.findAll();
+    await this.sessionsGateway.getSessions(sessions);
+    return session;
   }
 
   async findLastSessionByUserId(userId: string): Promise<SessionDocument> {
@@ -30,9 +35,13 @@ export class SessionService {
 
   async deleteSessionByUserId(userId: string): Promise<void> {
     await this.sessionModel.deleteOne({ userId });
+    const sessions = await this.findAll();
+    await this.sessionsGateway.getSessions(sessions);
   }
 
   async delete(sessionId: string): Promise<void> {
     await this.sessionModel.findByIdAndDelete(sessionId);
+    const sessions = await this.findAll();
+    await this.sessionsGateway.getSessions(sessions);
   }
 }
