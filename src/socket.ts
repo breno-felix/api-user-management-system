2cad3b1857bc33ff31bd1d2ef013b18b7eb3d87f
Breno Felix <brenodev.felix@gmail.com>
2023-04-23 22:45:42 -0300

    :wrench:	chore: config Socket server.

---
+++ src/socket.ts
@@ -0,0 +1,11 @@
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+export { httpServer, io };
